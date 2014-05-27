ef5d57ffc74f54802c5ad23ea4e209bace0c3da1
Maitri Kashyap <maitri.kashyap@gmail.com>
2014-05-27 10:44:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
-#To do
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name='PhotoSpread',
+    version='0.0.1',
+    author='Catherine Warren, Linh Tran, Maitri Kashyap',
+    author_email='pytest.photo@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://pypi.python.org/pypi/PhotoSpread/',
+    license='LICENSE.txt',
+    description='A Django-based project for organizing and sharing photos',
+    long_description=open('Photospread/README.md').read(),
+    install_requires=[
+        "Django == 1.6.2",
+        "South==0.8.4"
+        "Pillow==2.4.0"
+        "argparse==1.2.1",
+        "admin-multiupload==0.1"
+        "django-registration==1.0"
+        "wsgiref==0.1.2",
+    ],
+)
